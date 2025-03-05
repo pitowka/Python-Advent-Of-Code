7b61c8273a28e0866bc1de94121dd1bb970f8280
Peter Varchol <peter.varchol@plaut.sk>
2025-03-06 00:15:06 +0100

    2015, day4

---
+++ 2015/day4.py
@@ -0,0 +1,22 @@
+import hashlib
+from itertools import count
+
+puzzle_input: str = 'ckczppom'
+
+
+def part1() -> None:
+    idx: int = 0
+    while not (hashlib.md5(f'{puzzle_input}{idx}'.encode()).hexdigest().startswith('00000')):
+        idx += 1
+    print(idx)
+
+
+def part2() -> None:
+    idx: int = 0
+    while not (hashlib.md5(f'{puzzle_input}{idx}'.encode()).hexdigest().startswith('000000')):
+        idx += 1
+    print(idx)
+
+
+part1()
+part2()
